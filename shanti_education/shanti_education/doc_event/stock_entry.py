56e14595991aa8e0fc938563b82147a5e1e1b17e
FinByz <info@finbyz.tech>
2024-05-02 18:34:52 +0530

    Se outgoig qty changes

---
+++ shanti_education/shanti_education/doc_event/stock_entry.py
@@ -0,0 +1,20 @@
+from frappe import _
+
+def on_update(self, method):
+	out_and_in_qty(self)
+
+def out_and_in_qty(self):
+    # ongoing qty total
+    total_outgoing_qty = 0.0
+    total_incoming_qty = 0.0
+    for item in self.items:
+        if  item.is_finished_item == 0:
+            total_outgoing_qty += item.qty
+    self.total_outgoing_qty = total_outgoing_qty
+    # incoming qty total
+    for item in self.items:
+        if  item.is_finished_item == 1:
+            total_incoming_qty += item.qty
+    self.total_incoming_qty = total_incoming_qty
+
+
