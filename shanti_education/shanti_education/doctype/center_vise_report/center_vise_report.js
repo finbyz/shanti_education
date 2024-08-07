54a044c20e125f642fefb5399569d9087200da7f
finbyz <info@finbyz.tech>
2024-08-07 09:48:32 +0530

    New doctype add NB

---
+++ shanti_education/shanti_education/doctype/center_vise_report/center_vise_report.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, FINBYZ TECH PVT LTD and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Center Vise Report', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ shanti_education/shanti_education/doctype/center_vise_report/test_center_vise_report.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, FINBYZ TECH PVT LTD and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestCenterViseReport(FrappeTestCase):
+	pass
